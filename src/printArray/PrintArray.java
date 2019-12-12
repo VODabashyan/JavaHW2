6ccb11c3952194413e25acd4c46aa2425328c800
Vahe Odabashyan <vaheodabashyan@gmail.com>
2019-12-12 13:33:24 +0400

    Problem 1.1 declare and print an array

---
+++ src/printArray/PrintArray.java
@@ -0,0 +1,11 @@
+package printArray;
+//declare and print an array
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 2, 1200, 300, 21, -30};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
