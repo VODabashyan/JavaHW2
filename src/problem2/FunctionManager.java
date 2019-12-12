0f9660f009f72addece256e0dfa68a9deb53d3db
Vahe Odabashyan <vaheodabashyan@gmail.com>
2019-12-13 00:30:39 +0400

    added 'maxLong()' function

--- src/problem2/FunctionManager.java
+++ src/problem2/FunctionManager.java
@@ -14,4 +14,9 @@ public class FunctionManager {
         String result = x > y ? "Yes" : "No";
         System.out.println(result);
     }
+
+    public void maxLong(long x, long y, long z) {
+        long result = x > y ? (x > z ? x : z) : (y > z ? y : z);
+        System.out.println(result);
+    }
 }
